c6b6f90245a5992861b91cac7cd61db666d2adcf
LEONG98 <leonng0816@gmail.com>
2019-11-25 14:26:52 +0800

    init

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCalculate.setOnClickListener(){
+            calculateBMI()
+        }
+
+        btnReset.setOnClickListener(){
+            reset()
+        }
+    }
+
+    private fun calculateBMI(){
+        try {
+        val weight:Double = txtWeight.text.toString().toDouble()
+        val height:Double = txtHeight.text.toString().toDouble()
+
+            val value:Double = weight / (height * height)
+            val BMI:String
+
+            if(value <= 18.5){
+                txtBMI.text = "(Underweight)"
+                imgBMI.setImageResource(R.drawable.under)
+            }else if(value <= 24.9){
+                txtBMI.text = "(Normal)"
+                imgBMI.setImageResource(R.drawable.normal)
+            }else{
+                txtBMI.text = "(Overweight)"
+               imgBMI.setImageResource(R.drawable.over)
+            }
+
+            txtValue.text = "BMI = %.2f".format(value)
+        }
+        catch (e:Exception){
+            val toast:Toast = Toast.makeText(this, "Invalid Input", Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
+        }
+    }
+
+    private fun reset(){
+        txtHeight.text = null
+        txtWeight.text = null
+        txtValue.text = null
+        txtBMI.text = null
+        imgBMI.setImageResource(R.drawable.empty)
+        Toast.makeText(applicationContext, "Reset Completed", Toast.LENGTH_SHORT).show()
+    }
+}
